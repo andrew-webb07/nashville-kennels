a3cc7a29f8749997ba2f1e70bd72137c0eeab43f
Andrew Webb <webba@ihopu.org>
2021-05-28 11:56:48 -0500

    displayed basics home page

---
+++ src/components/employee/Employee.js
@@ -0,0 +1,10 @@
+import React from "react"
+import "./Employee.css"
+
+export const Employee = () => (
+    <section className="employee">
+        <h3 className="employee__name">Emma Beaton</h3>
+        <div className="employee__Company">Nashville Kennels North
+        </div>
+    </section>
+)
---
+++ src/components/location/Location.js
@@ -0,0 +1,10 @@
+import React from "react"
+import "./Location.css"
+
+export const Location = () => (
+    <section className="location">
+        <h3 className="location__name">Emma Beaton</h3>
+        <div className="location__Company">Nashville Kennels North
+        </div>
+    </section>
+)
