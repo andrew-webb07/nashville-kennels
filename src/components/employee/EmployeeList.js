891d5e2969f1c073b5f26d45843440d39d25f11c
Andrew Webb <webba@ihopu.org>
2021-06-01 14:00:27 -0400

    added employees to the DOM

---
+++ src/components/employee/EmployeeList.js
@@ -0,0 +1,34 @@
+import React, { useContext, useEffect } from "react"
+import { EmployeeContext } from "./EmployeeProvider"
+import "./Employee.css"
+
+export const EmployeeList = () => {
+  
+  const { employees, getEmployees } = useContext(EmployeeContext)
+
+  useEffect(() => {
+    console.log("EmployeeList: useEffect - getEmployees")
+    getEmployees()
+  }, [])
+
+
+  return (
+    <section className="employees">
+      {console.log("EmployeeList: Render", employees)}
+      {
+        employees.map(employee => {
+          return (
+            <div className="employee" id={`Employee--${employee.id}`}>
+              <div className="employee__name">
+                Name: { employee.name }
+              </div>
+              {/* <div className="employee__location">
+                Address: { employee.location }
+              </div> */}
+            </div>
+          )
+        })
+      }
+    </section>
+  )
+}
